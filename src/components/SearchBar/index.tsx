1ddd6f4ebed65e78d256529100062321e4eda4be
Daniela C. Barbosa <contato@danielacb.com>
2021-05-20 23:51:07 -0300

    create SearchBar component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './styles';
+
+const SearchBar: React.FC = () => {
+  return <S.Input type="text" placeholder="Busque por algo" />;
+};
+
+export default SearchBar;
---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+export const Input = styled.input(
+  ({ theme }) => css`
+    background-color: ${theme.colors.white + '16'};
+    border-radius: 0.5rem;
+    border: 0;
+    color: ${theme.colors.white + '64'};
+    font-size: 1rem;
+    line-height: 1.5rem;
+    padding: 1rem 0.875rem;
+    transition: 0.4s;
+
+    outline: 2px;
+    outline-color: transparent;
+
+    &::placeholder {
+      color: ${theme.colors.white + '64'};
+      font-size: 1rem;
+      line-height: 1.5rem;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: ${theme.colors.white + '24'};
+    }
+  `
+);
